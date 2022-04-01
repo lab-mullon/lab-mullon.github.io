979fc2e763121515a03e8bf3dd400a91acea3191
vsudbrack <vsud@bio-49-189.unil.ch>
2022-04-01 14:09:10 +0200

    Update code

--- docs/exam_program.R
+++ docs/exam_program.R
@@ -9,12 +9,12 @@
 # p = survival probability from age 1 to age 2
 # u = mutation rate
 # omega = intensity of selection
-# sig_u = size of the mutation step
+# sigma = size of the mutation step
 # Tf = number of timesteps
 
 ### Simulation program
 
-MA=function(n, b0, p, u, omega, sigma, Tf, mes)
+MA=function(n, b0, p, u, omega, sigma, Tf)
 {
   A=rep(1,n) # vector of ages
   Z=matrix(0, nrow=n, ncol=2) # matrix of phenotype, each line is an individual, one phenotype per age-class.
@@ -23,41 +23,42 @@ MA=function(n, b0, p, u, omega, sigma, Tf, mes)
   # At each timestep,
   for(t in 1:Tf) 
   {
-    fec=rep(0,n)
+    
+    fec=rep(0,n) # Create a vector of size n containing zeroes.
     for(i in 1:n)
     {
-      fec[i] = b0*exp(-(Z[i, A[i]]^2)/omega) 
+      fec[i] = b0*exp(-(Z[i, A[i]]^2)/omega) ################# ?
     }
+
+    Zp = Z # Copy the parental population into the Zp vector.
     
-    Zp = Z 
-    
-    for(i in 1:n)
+    for(i in 1:n) # For each individual,
     {
-      if( (runif(1) > p) || (A[i] == 2) ) 
+      if( (runif(1) > p) || (A[i] == 2) ) ########### ?
       {
-        par=sample(x = c(1:n), size = 1, prob=fec) 
+        par=sample(x = c(1:n), size = 1, prob=fec) ######### ?
         
-        A[i] = 1 
-        Z[i,] = Zp[par,] 
+        A[i] = 1 # Offspring age is set to 1.
+        Z[i,] = Zp[par,] # Offspring inherits the parental trait values.
         
-        for(j in 1:2)
+        for(j in 1:2) # For each trait,
         {
-          if(runif(1) < u)
+          if(runif(1) < u) # Offspring mutates with probability u
           {
-            Z[i,j] = Z[i,j] + sigma*rnorm(n = 1, mean = 0, sd = 1)
+            Z[i,j] = Z[i,j] + sigma*rnorm(n = 1, mean = 0, sd = 1) # When it mutates, the new value is sampled in a Gaussian centered on the parental value.
           }
         }
       }
       else
       {
-        A[i] = A[i] + 1
+        A[i] = A[i] + 1 # Increase individual age by 1.
       }
       
-    }
+    } # End of loop over individuals
     
-    for(j in 1:2)
+    for(j in 1:2) # For each trait,
     {
-      R[t,j] = mean(Z[,j])
+      R[t,j] = mean( b0*exp(-(Z[,j]^2)/omega)  ) # Record mean fecundity and store it in the appropriate column and row in R.
     }
     
   } # End of loop over time.
@@ -68,3 +69,4 @@ MA=function(n, b0, p, u, omega, sigma, Tf, mes)
 ### Run simulations
 
 sim = MA(n=, b0=, p=, u=, omega=, sigma=, Tf=)
+    
