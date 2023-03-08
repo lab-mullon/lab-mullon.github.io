021d2162a6796d4ce9f48b12ac6b3db42dc421fc
Vitor Sudbrack <vitorsudbrack@gmail.com>
2023-03-08 10:01:07 +0100

    update code

--- docs/code1-2023.R
+++ docs/code1-2023.R
@@ -1,6 +1,6 @@
 ################################### Exercise sheet 1 ###################################
 
-rm(list=ls()) # Clear all stored variables.
+# rm(list=ls()) # Clear all stored variables.
 
 ### Summary of parameters
 # n0 = initial population size
@@ -16,15 +16,15 @@ DYN=function(n0, n_a, Fec, P, p0, nt, nmax) # Arguments that will be taken by th
   #graphics.off()  # Automatically close graphics windows (if any), comment out if needed.
   
   A=rep(1,n0)  # Initial population is made of n0 individuals of age 1.
-  Res=rep(0, n_a) # Initialise the results matrix
+  Res=matrix(0, ncol=n_a,nrow=nt) # Initialise the results matrix
   
   D=as.data.frame( table(A) ) # Make a table of the number of individuals in each age-class.
   for( i in 1:nrow(D))
   {
-    Res[i] = D$Freq[i] # Store these numbers in Res.
+    Res[1,i] = D$Freq[i] # Store these numbers in Res.
   }
   
-  for(t in 1:nt)
+  for(t in 2:nt)
   {
     # Reproduction
     Off=0 # Total number of offspring produced
@@ -45,7 +45,7 @@ DYN=function(n0, n_a, Fec, P, p0, nt, nmax) # Arguments that will be taken by th
     }
 
     # Survival of established individuals
-    AS=c()
+    # We replace deceased individuals by 'NA' and then remove NAs from the vector
     for( i in 1:length(A))
     {
       if(A[i] < n_a) # If they are younger than n_a (otherwise they die with probability 1),
@@ -53,41 +53,52 @@ DYN=function(n0, n_a, Fec, P, p0, nt, nmax) # Arguments that will be taken by th
         # Established individuals survive to the next time step according to their age-specific survival probability
         if( runif(1) < P[ A[i] ] )
         {
-          AS=c(AS, A[i] + 1) # If they survive, they are kept and their age increases by one.
+          A[i]= A[i] + 1 # If they survive, they are kept and their age increases by one.
+        }
+        else
+        {
+          A[i] = NA # Otherwise they are marked as dead.
         }
       }
+      else
+      {
+        A[i] = NA  # Marked as dead.
+      }
     }
-    
+    AS=A[!is.na(A)] # Survivors are all the individuals that are not marked with an NA.
     A=c(AO,AS) # Newly established individuals and survivors constitute the population at the next time step
     
-    if( length(A) == 0)
+    if( length(A) == 0) # If the population size reaches zero, we end the time-loop.
     {
-      tmp=rep(0, n_a)
-      Res=rbind(Res,tmp)
-      
-      break # If the population size reaches zero, we end the time-loop.
+      Res[t,]=rep(0, n_a)
+      break 
     }
-    else
+    else # Otherwise, we simply store results in Res.
     {
-      D=as.data.frame( table(A) ) # Store results in Res.
+      D=as.data.frame( table(A) ) 
       tmp=rep(0, n_a)
       for( i in 1:nrow(D))
       {
         tmp[ D$A[i] ] = D$Freq[i]
       }
-      Res=rbind(Res,tmp)
+      Res[t,]=tmp
     }
     
-    if( length(A) >= nmax )
+    if( length(A) >= nmax ) # If the population size reaches the size threshold, the simulation is halted.
     {
-      break # If the population size reaches the size threshold, the simulation is halted.
+      Res=Res[1:t,]
+      break
     }
 
   } # End of loop over time.
 
-  # Return the results matrix
-  return(Res)
+  # Some cosmetic changes to the results matrix
+  row.names(Res)=NULL
+  colnames(Res)=c("n1","n2","n3","n4","n5","n6")
+  
+  return(Res) # Return the results matrix
 }
 
 #Simulation results are stored in the 'sim' object.
-sim = DYN(n0=, n_a=, Fec=c(), P=c(), p0=, nt=, nmax = 100000)
+
+sim = DYN(n0= , n_a= , Fec= , P= , p0= , nt= , nmax = )
